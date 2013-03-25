57dd4fd5fd38a7b6695a808e0e8a15601bb48986
Ben Deutsch <ben@bendeutsch.de>
2013-03-25 15:54:26 +0100

    Map (tile) is now an object.

--- my_webgl.js
+++ my_webgl.js
@@ -7,25 +7,7 @@ var p_mat;
 var mv_mat;
 var n_mat;
 
-var map_heights = [];
-var map_position_buffer;
-var map_color_buffer;
-var map_index_buffer;
-var map_normal_buffer;
-var v_count = 0;
-
-var map_x = 255;
-var map_y = 255;
-
-function Map( ) {
-    this.heights = [];
-    this.position_buffer = null;
-    this.color_buffer = null;
-    this.index_buffer = null;
-    this.normal_buffer = null;
-    this.width = 128; // in tiles
-    this.height = 128; 
-}
+var map_tile;
 
 var camera = {
     rx: 0.1,
@@ -50,6 +32,103 @@ window.requestAnimFrame =
     window.requestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.webkitRequestAnimationFrame;
+
+function MapTile( origin_x, origin_y ) {
+    this.heights = [];
+    this.width = 128; // in tiles
+    this.height = 128; 
+    this.origin_x = origin_x;
+    this.origin_y = origin_y;
+    //console.log(origin_x, origin_y);
+
+    this.position_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.position_buffer);
+    var v = [ ];
+    var vc = [ ];
+    var wx, wy;
+    // remember: need one more, for the edges
+    for (var y=0; y<=this.height; y++) {
+        for (var x=0; x<=this.width; x++) {
+            wx = this.origin_x + x;
+            wy = this.origin_y + y;
+            var h;
+            h = map_height_at(wx, wy, false);
+            //h = (Math.sin(x / 5.0) + Math.sin(y / 5.0) + 2.0) / 4.0;
+            // Lighting will take care of this
+            //vc.push(1.0, 1.0, 1.0, 1.0);
+
+            // Mock
+            var terrain = h - rand_field(wx / 4.5, wy / 4.5) * 0.02;
+            if (terrain <= 0.0) {
+                vc.push(0.2, 0.2, 0.5, 1.0);
+            } else if (terrain < 0.02) {
+                vc.push(1.0, 1.0, 0.7, 1.0);
+            } else if (terrain < 0.15) {
+                vc.push(0.0, 1.0, 0.0, 1.0);
+            } else if (terrain < 0.35) {
+                vc.push(0.0, 0.7, 0.0, 1.0);
+            } else if (terrain < 0.5) {
+                vc.push(0.5, 0.5, 0.5, 1.0);
+            } else {
+                vc.push(1.0, 1.0, 1.0, 1.0);
+            }
+
+            h *= 50.0;
+            // h = 0-50, tall enough
+
+            this.heights.push(h);
+            v.push(wx, h, wy);
+            //vc.push((x%2)/2.0, (x+y)%4/4.0, (y%3)/3.0, 1.0);
+            //if (x == Math.floor(camera.t[0]) && y == Math.floor(camera.t[2])) {
+            //    camera.t[1] = h+1;
+            //}
+
+        }
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(v), gl.STATIC_DRAW);
+
+    this.color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vc), gl.STATIC_DRAW);
+
+    // normals
+    // edges are flat
+    this.normal_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.normal_buffer);
+    var vn = [ ];
+    var n_tmp = vec3.create();
+    for (var y=0; y<=this.height; y++) {
+        for (var x=0; x<=this.width; x++) {
+            wx = this.origin_x + x;
+            n_tmp = vec3.fromValues(
+                map_height_at(wx+1, wy, true) - map_height_at(wx-1, wy, true),
+                //this.heights[(x+1) + (this.width+1) * y] - this.heights[(x-1) + (this.width+1) * y],
+                1.0,
+                map_height_at(wx, wy+1, true) - map_height_at(wx, wy-1, true)
+                //this.heights[x + (this.width+1) * (y+1)] - this.heights[x + (this.width+1) * (y-1)]
+            );
+            vec3.normalize(n_tmp, n_tmp);
+            vn.push(n_tmp[0], n_tmp[1], n_tmp[2]);
+        }
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vn), gl.STATIC_DRAW);
+
+    this.index_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
+    var vi = [ ];
+    for (var y=0; y<this.height; y++) {
+        for (var x=0; x<this.width; x++) {
+            vi.push( (x+0) + (this.width+1)*(y+0));
+            vi.push( (x+0) + (this.width+1)*(y+1));
+            vi.push( (x+1) + (this.width+1)*(y+0));
+
+            vi.push( (x+1) + (this.width+1)*(y+0));
+            vi.push( (x+0) + (this.width+1)*(y+1));
+            vi.push( (x+1) + (this.width+1)*(y+1));
+        }
+    }
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vi), gl.STATIC_DRAW);
+}
 
 function shaders_loaded() {
     shader_program = gl.createProgram();
@@ -81,121 +160,8 @@ function shaders_loaded() {
 
 function make_map() {
 
-    map_position_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, map_position_buffer);
-    var v = [ ];
-    var vc = [ ];
-    // remember: need one more, for the edges
-    for (var y=0; y<=map_y; y++) {
-        for (var x=0; x<=map_x; x++) {
-            var h;
-            //h = (Math.sin(x / 5.0) + Math.sin(y / 5.0) + 2.0) / 4.0;
-            var dx = Math.abs(x - map_x / 2.0);
-            var dy = Math.abs(y - map_y / 2.0);
-            var d = Math.sqrt(dx * dx + dy * dy);
-            // boundaries, 0-1
-            var h_max = Math.min( 1.0 - (dx / (map_x/1.4)) , 1.0 - (dy / (map_y/1.4)) );
-            h_max = Math.pow(h_max, 1.5);
-            if (h_max < 0.0) { h_max = 0.0; }
-            h = 0;
-            h += rand_field(x / 25.0 , y / 25.0 ) * 0.75;
-            h += rand_field(x / 10.0 , y / 10.0 ) * 0.23;
-            h += rand_field(x / 2.0 , y / 2.0) * 0.02;
-            // h is 0-1
-            // create grooves!
-            //var h_min = 2.0 / (2.0 + rand_field(x / 30.0, y/30.0));
-            //if (h > h_min) {
-            //    h = h_min - (h - h_min);
-            //}
-            h = h * h_max - (1.0-h_max)*0.2;
-            // h is 0-1
+    map_tile = new MapTile(0, 0);
 
-            if (h < 0.0) { h = 0.0; }
-            // h = 0-1
-
-            // Lighting will take care of this
-            //vc.push(1.0, 1.0, 1.0, 1.0);
-
-            // Mock
-            var terrain = h - rand_field(x / 4.5, y / 4.5) * 0.02;
-            if (terrain <= 0.0) {
-                vc.push(0.2, 0.2, 0.5, 1.0);
-            } else if (terrain < 0.02) {
-                vc.push(1.0, 1.0, 0.7, 1.0);
-            } else if (terrain < 0.15) {
-                vc.push(0.0, 1.0, 0.0, 1.0);
-            } else if (terrain < 0.35) {
-                vc.push(0.0, 0.7, 0.0, 1.0);
-            } else if (terrain < 0.5) {
-                vc.push(0.5, 0.5, 0.5, 1.0);
-            } else {
-                vc.push(1.0, 1.0, 1.0, 1.0);
-            }
-
-            h *= 50.0;
-            // h = 0-50, tall enough
-
-            map_heights.push(h);
-            v.push(x, h, y);
-            //vc.push((x%2)/2.0, (x+y)%4/4.0, (y%3)/3.0, 1.0);
-            if (x == Math.floor(camera.t[0]) && y == Math.floor(camera.t[2])) {
-                camera.t[1] = h+1;
-            }
-
-        }
-    }
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(v), gl.STATIC_DRAW);
-
-    map_color_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, map_color_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vc), gl.STATIC_DRAW);
-
-    // normals
-    // edges are flat
-    map_normal_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, map_normal_buffer);
-    var vn = [ ];
-    var n_tmp = vec3.create();
-    for (var x=0; x<=map_x; x++) {
-        vn.push(0.0, 1.0, 0.0);
-    }
-    for (var y=1; y<map_y; y++) {
-        vn.push(0.0, 1.0, 0.0);
-        for (var x=1; x<map_x; x++) {
-            n_tmp = vec3.fromValues(
-                map_heights[(x+1) + (map_x+1) * y] - map_heights[(x-1) + (map_x+1) * y],
-                1.0,
-                map_heights[x + (map_x+1) * (y+1)] - map_heights[x + (map_x+1) * (y-1)]
-            );
-            vec3.normalize(n_tmp, n_tmp);
-            vn.push(n_tmp[0], n_tmp[1], n_tmp[2]);
-        }
-        vn.push(0.0, 1.0, 0.0);
-    }
-    for (var x=0; x<=map_x; x++) {
-        vn.push(0.0, 1.0, 0.0);
-    }
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vn), gl.STATIC_DRAW);
-
-    map_index_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, map_index_buffer);
-    var vi = [ ];
-    for (var y=0; y<map_y; y++) {
-        for (var x=0; x<map_x; x++) {
-            vi.push( (x+0) + (map_x+1)*(y+0));
-            vi.push( (x+0) + (map_x+1)*(y+1));
-            vi.push( (x+1) + (map_x+1)*(y+0));
-
-            vi.push( (x+1) + (map_x+1)*(y+0));
-            vi.push( (x+0) + (map_x+1)*(y+1));
-            vi.push( (x+1) + (map_x+1)*(y+1));
-
-            v_count += 6;
-        }
-    }
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vi), gl.STATIC_DRAW);
-
-    window.requestAnimFrame(draw);
 }
 
 function do_webgl () {
@@ -268,6 +234,7 @@ function do_webgl () {
     //console.log('requesting noise.png');
 
     //window.mozRequestAnimationFrame(draw);
+    window.requestAnimFrame(draw);
 }
 
 function noise_loaded() {
@@ -327,20 +294,20 @@ function draw() {
     vec3.set(move_vec, mv_side, mv_up, mv_forward);
     vec3.transformMat3(move_vec, move_vec, move_mat);
     vec3.add(camera.t, camera.t, move_vec);
-    if (camera.t[1] <= map_height_at(camera.t[0], camera.t[2]) + 1.0 )  {
-        camera.t[1] = map_height_at(camera.t[0], camera.t[2]) + 1.0;
+    if (camera.t[1] <= map_height_at(camera.t[0], camera.t[2], true) + 1.0 )  {
+        camera.t[1] = map_height_at(camera.t[0], camera.t[2], true) + 1.0;
     }
 
     // reuse move_vec as -t
     vec3.negate(move_vec, camera.t)
     mat4.translate(mv_mat, mv_mat, move_vec);
 
-    if (map_index_buffer) {
-        gl.bindBuffer(gl.ARRAY_BUFFER, map_position_buffer);
+    if (map_tile) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, map_tile.position_buffer);
         gl.vertexAttribPointer(shader_program.v_pos, 3, gl.FLOAT, false, 0, 0);
-        gl.bindBuffer(gl.ARRAY_BUFFER, map_color_buffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, map_tile.color_buffer);
         gl.vertexAttribPointer(shader_program.v_col, 4, gl.FLOAT, false, 0, 0);
-        gl.bindBuffer(gl.ARRAY_BUFFER, map_normal_buffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, map_tile.normal_buffer);
         gl.vertexAttribPointer(shader_program.v_nor, 3, gl.FLOAT, false, 0, 0);
 
         gl.uniformMatrix4fv(shader_program.p_mat, false, p_mat);
@@ -357,9 +324,8 @@ function draw() {
         gl.bindTexture(gl.TEXTURE_2D, noise_tex);
         gl.uniform1i(shader_program.noise_tex, 0);
 
-        //gl.drawArrays(gl.TRIANGLES, 0, v_count);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, map_index_buffer);
-        gl.drawElements(gl.TRIANGLES, v_count, gl.UNSIGNED_SHORT, 0);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, map_tile.index_buffer);
+        gl.drawElements(gl.TRIANGLES, ( map_tile.width * map_tile.height * 6), gl.UNSIGNED_SHORT, 0);
     }
 }
 
@@ -471,6 +437,7 @@ function pointerlockChange() {
 var rand_map = null;
 
 function rand_field(x, y) {
+    //console.log(x, y);
     var w = 10;
     var h = 10;
     if (!rand_map) {
@@ -511,8 +478,43 @@ function rand_field(x, y) {
     //return Math.random();
 }
 
-function map_height_at(x, y) {
-    if (x <= 0.0 || y <= 0.0 || x >= map_x || y >= map_y) {
+function map_height_at(x, y, scaled) {
+    //console.log(x, y);
+    var dx = Math.abs(x - 255 / 2.0);
+    var dy = Math.abs(y - 255 / 2.0);
+    var d = Math.sqrt(dx * dx + dy * dy);
+    // boundaries, 0-1
+    var h_max = Math.min( 1.0 - (dx / (255/1.4)) , 1.0 - (dy / (255/1.4)) );
+    h_max = Math.pow(h_max, 1.5);
+    if (h_max < 0.0) { h_max = 0.0; }
+    var h = 0.0;
+    h += rand_field(x / 25.0 , y / 25.0 ) * 0.75;
+    h += rand_field(x / 10.0 , y / 10.0 ) * 0.23;
+    h += rand_field(x / 2.0 , y / 2.0) * 0.02;
+    // h is 0-1
+    // create grooves!
+    //var h_min = 2.0 / (2.0 + rand_field(x / 30.0, y/30.0));
+    //if (h > h_min) {
+    //    h = h_min - (h - h_min);
+    //}
+    h = h * h_max - (1.0-h_max)*0.2;
+    // h is 0-1
+
+    if (h < 0.0) { h = 0.0; }
+    // h = 0-1
+
+    if (scaled) {
+        return h * 50.0;
+    } else {
+        return h;
+    }
+}
+
+function discrete_map_height_at(x, y) {
+    if (!map_tile) {
+        return 0.0;
+    }
+    if (x <= 0.0 || y <= 0.0 || x >= map_tile.width || y >= map_tile.height) {
         return 0.0; // sea level
     }
     var x0 = Math.floor(x);
@@ -521,8 +523,8 @@ function map_height_at(x, y) {
     var y1 = y0 + 1;
     var dx = x - x0;
     var dy = y - y0;
-    return (1-dx)*(1-dy)*map_heights[x0 + (map_x+1) * y0] +
-        (dx)*(1-dy)*map_heights[x1 + (map_x+1) * y0] +
-        (1-dx)*(dy)*map_heights[x0 + (map_x+1) * y1] +
-        (dx)*(dy)*map_heights[x1 + (map_x+1) * y1];
+    return (1-dx)*(1-dy)*map_tile.heights[x0 + (map_tile.width+1) * y0] +
+        (dx)*(1-dy)*map_tile.heights[x1 + (map_tile.width+1) * y0] +
+        (1-dx)*(dy)*map_tile.heights[x0 + (map_tile.width+1) * y1] +
+        (dx)*(dy)*map_tile.heights[x1 + (map_tile.width+1) * y1];
 }
